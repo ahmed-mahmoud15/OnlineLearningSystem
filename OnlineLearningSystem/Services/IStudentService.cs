9ef1ce521fad9ccb906341aa2d268811fc6a2056
hamata <ahmed.adham.1543@gmail.com>
2025-08-19 20:12:43 +0300

    need to make sure which parameters should the service take

--- OnlineLearningSystem/Services/IStudentService.cs
+++ OnlineLearningSystem/Services/IStudentService.cs
@@ -5,5 +5,13 @@ namespace OnlineLearningSystem.Services
     public interface IStudentService
     {
         public Task AddStudentAsync(Student student, IFormFile file);
+
+        public Task LikeCourse(int studentId, int courseId);
+
+        public Task FollowInstructor(int studentId, int instructorId);
+
+        public Task EnrollInCourse(int studentId, int courseId);
+
+        public Task MakePayment(int studentId);
     }
 }
